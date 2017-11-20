9a56ab4ce670ec957449bdf108b4de2144e96319
unknown <1666395210@qq.com>
2017-11-20 18:50:43 +0800

    url美化

---
+++ backend/models/SiteModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * File_Name: SiteModel.php
+ * User: 刘亚博
+ * Date: 2017/11/20
+ * Time: 14:07
+ * 摘要:
+ */
+
+namespace backend\model;
+
+use yii\base\Model;
+
+class SiteModel extends Model
+{
+    public function test()
+    {
+
+    }
+}
